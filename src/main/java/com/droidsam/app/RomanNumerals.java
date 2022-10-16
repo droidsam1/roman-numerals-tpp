608914d76cd20b97f50307dad5bbe301179f4686
droidsam1 <droidsam@gmail.com>
2022-10-16 08:54:44 +0200

    refactor: reduce complexity by removing array

--- src/main/java/com/droidsam/app/RomanNumerals.java
+++ src/main/java/com/droidsam/app/RomanNumerals.java
@@ -3,16 +3,12 @@ package com.droidsam.app;
 public class RomanNumerals {
     public static String convertFromArabicNumber(int arabicNumber) {
 
-        String [] romanRepresentations = new String[10];
-        romanRepresentations[4]="IV";
-        romanRepresentations[5]="V";
-        romanRepresentations[6]="VI";
-        romanRepresentations[7]="VII";
-        romanRepresentations[8]="VIII";
+        if (arabicNumber == 4) {
+            return "IV";
+        }
 
-
-        if (romanRepresentations[arabicNumber] !=null) {
-            return romanRepresentations[arabicNumber];
+        if (arabicNumber >= 5) {
+            return "V" + "I".repeat(arabicNumber - 5);
         }
 
         return "I".repeat(arabicNumber);
