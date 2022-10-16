7e3c22ef03c25b51d137da4fed00901567e6411a
droidsam1 <droidsam@gmail.com>
2022-10-16 08:57:24 +0200

    add roman representation for ten

--- src/main/java/com/droidsam/app/RomanNumerals.java
+++ src/main/java/com/droidsam/app/RomanNumerals.java
@@ -3,19 +3,21 @@ package com.droidsam.app;
 public class RomanNumerals {
     public static String convertFromArabicNumber(int arabicNumber) {
 
+        if (arabicNumber == 10) {
+            return "X";
+        }
 
         if (arabicNumber == 9) {
             return "IX";
-        }
-
-        if (arabicNumber == 4) {
-            return "IV";
         }
 
         if (arabicNumber >= 5) {
             return "V" + "I".repeat(arabicNumber - 5);
         }
 
+        if (arabicNumber == 4) {
+            return "IV";
+        }
 
         return "I".repeat(arabicNumber);
     }
