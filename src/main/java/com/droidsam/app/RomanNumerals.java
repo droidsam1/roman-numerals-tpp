a0ff2369413f4eb9e2c371099c730bc86e8c149b
droidsam1 <droidsam@gmail.com>
2022-10-16 08:55:51 +0200

    add roman representation for nice

--- src/main/java/com/droidsam/app/RomanNumerals.java
+++ src/main/java/com/droidsam/app/RomanNumerals.java
@@ -3,6 +3,11 @@ package com.droidsam.app;
 public class RomanNumerals {
     public static String convertFromArabicNumber(int arabicNumber) {
 
+
+        if (arabicNumber == 9) {
+            return "IX";
+        }
+
         if (arabicNumber == 4) {
             return "IV";
         }
@@ -10,6 +15,7 @@ public class RomanNumerals {
         if (arabicNumber >= 5) {
             return "V" + "I".repeat(arabicNumber - 5);
         }
+
 
         return "I".repeat(arabicNumber);
     }
--- src/test/java/com/droidsam/app/RomanNumeralsTest.java
+++ src/test/java/com/droidsam/app/RomanNumeralsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class RomanNumeralsTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"1, I", "2,II", "3, III", "4, IV", "5, V", "6, VI", "7, VII", "8, VIII"})
+    @CsvSource(value = {"1, I", "2,II", "3, III", "4, IV", "5, V", "6, VI", "7, VII", "8, VIII", "9, IX"})
     public void shouldConvertToRoman(Integer arabic, String expectedRepresentation) {
         Assertions.assertEquals(expectedRepresentation, RomanNumerals.convertFromArabicNumber(arabic));
     }
