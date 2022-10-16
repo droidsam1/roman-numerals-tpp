3c9454f74a9cd2f9bedbd27dcffb32b6a00f803d
droidsam1 <droidsam@gmail.com>
2022-10-16 08:12:52 +0200

    add roman representation for six

--- src/main/java/com/droidsam/app/RomanNumerals.java
+++ src/main/java/com/droidsam/app/RomanNumerals.java
@@ -2,11 +2,15 @@ package com.droidsam.app;
 
 public class RomanNumerals {
     public static String convertFromArabicNumber(int arabicNumber) {
-        if(arabicNumber>=5){
+        if (arabicNumber >= 6) {
+            return "VI";
+        }
+
+        if (arabicNumber == 5) {
             return "V";
         }
 
-        if(arabicNumber == 4){
+        if (arabicNumber == 4) {
             return "IV";
         }
         return "I".repeat(arabicNumber);
